ad13d62d975dcb190903fbecfa474c70561a849b
Shubham Kumar Bansal <shub@shubkb.com>
2024-10-19 15:58:53 +0530

    first commit

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 
-@app.post("/students", status_code=201)
+@app.get("/students", status_code=201)
 async def create_student():
 	return {"id": "0"}
